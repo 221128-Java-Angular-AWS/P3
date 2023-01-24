cb56cbab347108d2c869f0e393da0f08c23a8dd5
Braymann <braymann00@gmail.com>
2023-01-24 10:20:41 -0600

    creating new tables and adding empty files for controllers, services, and repos

---
+++ e-commerce-backend-main/src/main/java/com/revature/controllers/ReviewController.java
@@ -0,0 +1,11 @@
+package com.revature.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
+public class ReviewController {
+}
