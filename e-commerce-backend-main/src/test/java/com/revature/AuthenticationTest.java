59e89d6b997e345c24014b01d01a10ec30c6588a
kklimov1 <kirillvlklimov@gmail.com>
2023-01-24 10:52:27 -0600

    Commit before pull

---
+++ e-commerce-backend-main/src/test/java/com/revature/AuthenticationTest.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//Maybe necessary @RunWith(SpringRunner.class)
+public class AuthenticationTest {
+}
