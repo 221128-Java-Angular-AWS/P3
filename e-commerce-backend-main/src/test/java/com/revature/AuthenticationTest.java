0091fc2bdbe3b07ce0cda99ca61096e57d753e1f
kklimov1 <kirillvlklimov@gmail.com>
2023-01-24 12:19:10 -0600

    Added yml

--- e-commerce-backend-main/src/test/java/com/revature/AuthenticationTest.java
+++
@@ -1,8 +0,0 @@
-package com.revature;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-//Maybe necessary @RunWith(SpringRunner.class)
-public class AuthenticationTest {
-}
