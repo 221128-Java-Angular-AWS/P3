e3d6398e2c7964e04a575c7f861eb981af66b9b8
Travis Pomeroy <pomeroyzg@gmail.com>
2023-01-27 12:34:51 -0800

    Quick fix to the product model

--- e-commerce-backend-main/src/main/resources/data.sql
+++ e-commerce-backend-main/src/main/resources/data.sql
@@ -1,93 +1,93 @@
-INSERT INTO product (id, genre, quantity, price, description, image, name) VALUES (
+INSERT INTO product (id, quantity, price, description, image, name, genre) VALUES (
     1,
-    'test',
     10,
     20.00,
     'A nice pair of headphones',
     'https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp',
-    'Headphones'
+    'Headphones',
+    'test'
 ),
 (
     2,
-    'test',
     5,
     45.00,
     'A nice TeeShirt',
     'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
-    'TeeShirt'
+    'TeeShirt',
+    'test'
 ),
 (
     3,
-    'Other',
     20,
     2.50,
     'A reusable shopping bag',
     'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
-    'Shopping Bag'
+    'Shopping Bag',
+    'Other'
 ),
 (
     4,
-    'test',
     20,
     10.00,
     'A fancy cap for a fancy person',
     'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
-    'Baseball Cap'
+    'Baseball Cap',
+    'test'
 ),
 (
     5,
-    'Other',
     3,
     80.00,
     'A nice coat',
     'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
-    'Coat'
+    'Coat',
+    'Other'
 ),
-(
-    6,
-    'test',
-    10,
-    20.00,
-    'A nice pair of headphones',
-    'https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp',
-    'Headphones'
-),
-(
-    7,
-    'test',
-    5,
-    45.00,
-    'A nice TeeShirt',
-    'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
-    'TeeShirt'
-),
-(
-    8,
-    'Other',
-    20,
-    2.50,
-    'A reusable shopping bag',
-    'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
-    'Shopping Bag'
-),
-(
-    9,
-    'test',
-    20,
-    10.00,
-    'A fancy cap for a fancy person',
-    'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
-    'Baseball Cap'
-),
-(
-    10,
-    'Other',
-    3,
-    80.00,
-    'A nice coat',
-    'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
-    'Coat'
-);
+(    6,
+     10,
+     20.00,
+     'A nice pair of headphones',
+     'https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp',
+     'Headphones',
+     'test'
+ ),
+ (
+     7,
+     5,
+     45.00,
+     'A nice TeeShirt',
+     'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
+     'TeeShirt',
+     'test'
+ ),
+ (
+     8,
+     20,
+     2.50,
+     'A reusable shopping bag',
+     'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
+     'Shopping Bag',
+     'Other'
+ ),
+ (
+     9,
+     20,
+     10.00,
+     'A fancy cap for a fancy person',
+     'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
+     'Baseball Cap',
+     'test'
+ ),
+ (
+     10,
+     3,
+     80.00,
+     'A nice coat',
+     'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
+     'Coat',
+     'Other'
+ );
+
 
 INSERT INTO users (id, email, password, first_name, last_name) VALUES (
     1,
