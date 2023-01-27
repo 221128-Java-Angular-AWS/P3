441e0719214eb8e8c786a8684b602a8f738593f8
kklimov1 <kirillvlklimov@gmail.com>
2023-01-27 10:23:28 -0600

    Added new data

--- e-commerce-backend-main/src/main/java/com/revature/models/User.java
+++ e-commerce-backend-main/src/main/java/com/revature/models/User.java
@@ -25,19 +25,19 @@ public class User {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     @JsonManagedReference(value = "order_user")
-    public List<Order> orders;
+    List<Order> orders;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     @JsonManagedReference(value = "review_user")
-    public List<Review> reviews;
+    List<Review> reviews;
 
     @OneToMany(mappedBy = "user")
     @JsonManagedReference(value = "cart_user")
-    public List<Cart> cart;
+    List<Cart> cart;
 
     @OneToMany(mappedBy = "user")
     @JsonManagedReference(value = "wish_user")
-    public List<WishList> wishList;
+    List<WishList> wishList;
 
     public User(int id, String email, String password, String firstName, String lastName) {
         this.id = id;
@@ -51,6 +51,46 @@ public class User {
         this.email = email;
         this.password = password;
         this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
