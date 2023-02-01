5917bc4613252f3b99b9c48f347e964d51c6d10d
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-02-01 17:18:44 -0500

    testing change to prod env

--- e-commerce-frontend-angular-main/src/environments/environment.prod.ts
+++ e-commerce-frontend-angular-main/src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   baseUrl: "http://ec2-3-133-147-18.us-east-2.compute.amazonaws.com:7777",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    // 'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin': '*',
   }
 };
