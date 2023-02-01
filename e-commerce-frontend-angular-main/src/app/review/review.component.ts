4bce58c23b72998c530136c2bab46d96cfaef04b
kklimov1 <kirillvlklimov@gmail.com>
2023-02-01 15:32:36 -0600

    Added star ratings

--- e-commerce-frontend-angular-main/src/app/review/review.component.ts
+++ e-commerce-frontend-angular-main/src/app/review/review.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-review',
@@ -7,14 +6,14 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
-  output?: string;
-  constructor(private formBuilder: FormBuilder) { }
+  review?: number;
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(): void {
-    
+  assignReview(n: number): void {
+    this.review = n;
   }
 
 }
