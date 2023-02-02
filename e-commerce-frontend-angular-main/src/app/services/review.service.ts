7a43f1ca0a8b805fdbec23bad6c2e6a23c27dbea
kklimov1 <kirillvlklimov@gmail.com>
2023-02-02 12:23:45 -0600

    Added service for reviews 1

---
+++ e-commerce-frontend-angular-main/src/app/services/review.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  constructor() { }
+}
