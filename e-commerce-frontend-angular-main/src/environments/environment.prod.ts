d9ddc331602246481b4d35425689f93173fb6e03
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-02-03 18:01:29 -0500

    testing wild card

--- e-commerce-frontend-angular-main/src/environments/environment.prod.ts
+++ e-commerce-frontend-angular-main/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   baseUrl: "http://ec2-18-219-177-77.us-east-2.compute.amazonaws.com:7777",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://p3-static-hosting.s3-website.us-east-2.amazonaws.com'
+    'Access-Control-Allow-Origin': '*'
   }
 };
