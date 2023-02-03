3f817b0c231e728fe688870c6f63fd2b52356b87
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-02-03 15:58:44 -0500

    testing auth header expose

--- e-commerce-backend-main/src/main/java/com/revature/controllers/ReviewController.java
+++ e-commerce-backend-main/src/main/java/com/revature/controllers/ReviewController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/review")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://p3-static-hosting.s3-website.us-east-2.amazonaws.com"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://p3-static-hosting.s3-website.us-east-2.amazonaws.com"}, allowCredentials = "true", exposedHeaders = "Authorization")
 public class ReviewController {
 }
