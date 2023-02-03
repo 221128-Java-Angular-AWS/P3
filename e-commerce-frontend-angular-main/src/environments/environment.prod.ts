57dd90c1640805127477bb0c51fe749fe20faa88
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-02-03 18:47:06 -0500

    reverting to previous build

--- e-commerce-frontend-angular-main/src/environments/environment.prod.ts
+++ e-commerce-frontend-angular-main/src/environments/environment.prod.ts
@@ -4,7 +4,6 @@ export const environment = {
   baseUrl: "http://ec2-18-219-177-77.us-east-2.compute.amazonaws.com:7777",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Cookie': 'SameSite=None; Secure'
+    'Access-Control-Allow-Origin': '*'
   }
 };
