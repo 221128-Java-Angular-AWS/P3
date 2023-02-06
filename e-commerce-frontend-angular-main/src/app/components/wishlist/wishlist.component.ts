b049e1d3baca07fbe9246690def21e1097feb272
Travis Morse <email@example.com>
2023-02-06 17:42:50 -0600

    refactored to properly find user, added error handling, added comments

--- e-commerce-frontend-angular-main/src/app/components/wishlist/wishlist.component.ts
+++ e-commerce-frontend-angular-main/src/app/components/wishlist/wishlist.component.ts
@@ -22,29 +22,21 @@ export class WishlistComponent implements OnInit {
   user!: User;
 
   ngOnInit(): void {
-    console.log('Initializing wishlist');
-    this.profileService.getUser().subscribe(
-      (resp) => {
-        this.user = resp;
-        this.wishListService.getWishListProducts(resp.id!).subscribe(
-          (resp) => this.products = resp,
-          (err) => console.log(err),
-          () => console.log("WishList products retrieved")
-        );
-      },
+    this.wishListService.getWishListProducts().subscribe(
+      (resp) => this.products = resp,
       (err) => console.log(err),
-      () => console.log("User retrieved")
+      () => {}
     );
   }
 
-  removeFromWishList(product: Product, userId: Number): void {
-    this.wishListService.removeFromWishList(product.id, userId)
-    .subscribe(() => console.log('Wishlist item deleted'));
+  removeFromWishList(product: Product): void {
+    this.wishListService.removeFromWishList(product.id)
+    .subscribe();
     this.products.splice(this.products.indexOf(product), 1);
   }
 
   addToCart(product: Product, userId: Number): void {
     this.productService.addCart(userId.valueOf(), product.id, 1)
-    .subscribe(() => console.log('Product added to cart from wishlist'));
+    .subscribe();
   }
 }
