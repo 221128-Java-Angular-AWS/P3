b1f05df664d8a2fbd967a5748580815bde5c94a7
Audie Ni <111254997+tasukaru@users.noreply.github.com>
2023-02-06 12:00:54 -0500

    testing Origin header

--- e-commerce-frontend-angular-main/src/environments/environment.prod.ts
+++ e-commerce-frontend-angular-main/src/environments/environment.prod.ts
@@ -4,9 +4,7 @@ export const environment = {
   baseUrl: "http://ec2-18-219-177-77.us-east-2.compute.amazonaws.com:7777",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'cross-site',
+    'Access-Control-Allow-Origin': 'http://p3-static-hosting.s3-website.us-east-2.amazonaws.com',
+    'Origin': 'http://ec2-18-219-177-77.us-east-2.compute.amazonaws.com:7777',
   }
 };
