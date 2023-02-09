134a99eb2f42c500dd1bd16ec47d8820a6072c5b
Braydensn <braydensn@gmail.com>
2023-02-09 11:00:21 -0600

    Adding doccumentation to the order and auth classes

--- e-commerce-backend-main/src/main/java/com/revature/repositories/OrderRepository.java
+++ e-commerce-backend-main/src/main/java/com/revature/repositories/OrderRepository.java
@@ -10,13 +10,26 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
-    //find all orders belonging to a user
+    /**
+     * Finds all orders belonging to a user
+     * @param userId The ID of the user whose orders are being retrieved
+     * @return The list of orders belonging to the user
+     */
     public List<Order> findByUserId(Integer userId);
 
-    //find 5 most recent orders belonging to a user
+    /**
+     * Retrieves the 5 most recent orders belonging to a user
+     * @param userId The ID of the user whose orders are being retrieved
+     * @return The list of the user's orders
+     */
     @Query(value = "SELECT * FROM orders WHERE user_id = :userId ORDER BY date_ordered DESC LIMIT 5;", nativeQuery = true)
     public List<Order> findByUserIdWithLimit(@Param("userId") Integer userId);
 
-    //find a specific order belonging to a user
+    /**
+     * Retrieves a specific order belonging to a user
+     * @param orderId The ID of the order being requested
+     * @param userId The ID of the user requesting the order
+     * @return The requested order
+     */
     public Order findByOrderIdAndUserId(Integer orderId, Integer userId);
 }
