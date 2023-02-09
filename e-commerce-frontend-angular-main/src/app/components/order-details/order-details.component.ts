5de2ae53608096ba0be42223305b44505d2cca8b
Braydensn <braydensn@gmail.com>
2023-02-09 11:28:52 -0600

    Adding doccumentation to order and auth classes on the front end

--- e-commerce-frontend-angular-main/src/app/components/order-details/order-details.component.ts
+++ e-commerce-frontend-angular-main/src/app/components/order-details/order-details.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Order } from 'src/app/models/order';
 import { OrdersService } from 'src/app/services/orders.service';
 
+/**
+ * This component is a detailed view of a single order.
+ * It shows information about the individual products in the order
+ */
 @Component({
   selector: 'app-order-details',
   templateUrl: './order-details.component.html',
@@ -15,6 +19,9 @@ export class OrderDetailsComponent implements OnInit {
 
   constructor(private ordersService: OrdersService, private route: ActivatedRoute) { }
 
+  /**
+   * This function retrieves the order information when the component is initialized
+   */
   ngOnInit(): void {
     const orderId = Number(this.route.snapshot.paramMap.get('id'));
     this.ordersService.getOrder(orderId).subscribe((order) => {
@@ -26,6 +33,11 @@ export class OrderDetailsComponent implements OnInit {
     })
   }
 
+  /**
+   * This function finds the total number of products in the order
+   * @param order The order containing all of the products
+   * @returns The total number of products
+   */
   getItemTotal = function(order: Order): number{
     if(order.products == null){return 0}
     let count: number = 0;
@@ -35,6 +47,11 @@ export class OrderDetailsComponent implements OnInit {
     return count;
   }
 
+  /**
+   * This function finds the total cost of all of the products in the order
+   * @param order The order containing all of the products
+   * @returns The total cost of the products
+   */
   getTotalCost = function(order: Order): number{
     if(order.products == null){return 0}
     let count: number = 0;
